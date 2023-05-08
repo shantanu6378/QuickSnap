b3c3bfcbcfa81bbba94910e4930715a5df56daf2
shantanu6378 <88345694+shantanu6378@users.noreply.github.com>
2023-05-08 04:31:25 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,27 +5,53 @@ import "./App.css";
 
 
 function DigitAnimation({ onAnimationFinish, duration }) {
-  const [number, setNumber] = useState(Math.floor(Math.random() * 4) + 1);
+  const [number, setNumber] = useState(null);
+  const [showCountdown, setShowCountdown] = useState(true);
+  const [countdown, setCountdown] = useState(3);
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      const newNumber = Math.floor(Math.random() * 4) + 1;
-      setNumber(newNumber);
-      setCount(prevCount => prevCount + 1);
-    }, duration);
+    if (showCountdown) {
+      const countdownInterval = setInterval(() => {
+        setCountdown(prevCountdown => prevCountdown - 1);
+      }, 1000);
+      
+      if (countdown === 0) {
+        clearInterval(countdownInterval);
+        setShowCountdown(false);
+        setNumber(Math.floor(Math.random() * 4) + 1);
+      }
+      
+      return () => clearInterval(countdownInterval);
+    } else {
+      const interval = setInterval(() => {
+        const newNumber = Math.floor(Math.random() * 4) + 1;
+        setNumber(newNumber);
+        setCount(prevCount => prevCount + 1);
+      }, duration);
 
-    if (count >= 10) {
-      clearInterval(interval);
-      onAnimationFinish();
+      if (count >= 10) {
+        clearInterval(interval);
+        onAnimationFinish();
+      }
+
+      return () => clearInterval(interval);
     }
-
-    return () => clearInterval(interval);
-  }, [count, onAnimationFinish, duration]);
+  }, [showCountdown, countdown, onAnimationFinish, duration, count]);
 
   return (
+    
     <div className="numbox">
-      <h1 style={{animationDuration: `${duration/1000}s`}}>{number}</h1>
+
+      {showCountdown && (
+        <div className="countdown">
+          <div className="crosshair" />
+          <h1 style = {{fontsize: "40px", color: 'rgb(19, 41, 75)'}}>{countdown}</h1>
+        </div>
+      )}
+      {!showCountdown && (
+        <h1 className="digit" style={{animationDuration: `${duration/1000}s`}}>{number}</h1>
+      )}
     </div>
   );
 }
@@ -52,18 +78,24 @@ function App() {
 
   return (
     <div className="main">
+     <h1 style={{ fontSize: '30px', color: 'rgb(176, 62, 28)', margin: '20px' }}>Follow the Stimulus</h1>
       <div className="button-container">
         {display && <GameDisplay onAnimationFinish={handleAnimationFinish} duration={intervalTime} />}
         <div className="slider-container">
           <label htmlFor="slider">Difficulty</label>
+          
           <input type="range"
           min="0"
           max="2500"
           value={3000 - intervalTime}
           onChange={handleIntervalChange}
           />
+        
+
         </div>
+        
         <button onClick={handleClick}>Play</button>
+        {/* <h1 style = {{fontsize: '30px', color:'red', margin: '70px 20px'}}>Follow the Stimulus</h1> */}
       </div>
     </div>
   );
